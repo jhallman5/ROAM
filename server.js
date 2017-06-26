d078d60a08b8f80227388eb84c973856b2a692e1
John <jhallman5@gmail.com>
2017-06-26 11:32:58 -0700

    basic server set up

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const server = express()
+
+const PORT = 3000
+
+server.set('view engine', 'ejs')
+server.set('views', path.join(__dirname, 'views'))
+
+server.use(bodyParser.urlencoded({ extended: false }))
+server.use(bodyParser.json())
+server.use('/static', express.static(path.join(__dirname, 'public')))
+
+server.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+server.listen(PORT,() => {
+  console.log('The Server is running on port', + PORT)
+})
