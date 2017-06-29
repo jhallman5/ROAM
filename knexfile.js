c4261631d6a9c313d5bb2329a0e04bcd58f3720b
John <jhallman5@gmail.com>
2017-06-29 16:23:18 -0700

    still trying heroku

--- knexfile.js
+++ knexfile.js
@@ -8,5 +8,15 @@ module.exports = {
     seeds: {
       directory: './database/seeds'
     }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
   }
 };
